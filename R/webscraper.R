3d51a55b2888a14c2ff83f576b24763fe437b395
Miguel Sebastian de la Mata <delamatas@appstate.edu>
2022-02-18 12:35:27 -0700

    cleaned xml first summary data into dataframe

--- R/webscraper.R
+++ R/webscraper.R
@@ -6,6 +6,18 @@ library(dplyr)
 # let's read a sample web page of of the denver property records
 sampleProperty <- read_html("https://www.denvergov.org/Property/realproperty/summary/0503111031031")
 
-sampleTable <- sampleProperty %>% html_elements('#property-info-bar') %>% html_table()
+summaryTable <- sampleProperty %>% html_elements('#property-info-bar') %>% html_table() 
 
-glimpse(sampleTable)
+# store our values
+scheduleNumber <- as.character(summaryTable[[1]][2,3])
+legalDescription <- as.character(summaryTable[[1]][2,4])
+propertyType <- as.character(summaryTable[[1]][2,5])
+taxDistrict <- as.character(summaryTable[[1]][2,6])
+
+summaryDF<- cbind(
+  scheduleNumber,
+  legalDescription,
+  propertyType,
+  taxDistrict
+)
+
